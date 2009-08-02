fded69f19f4a1dda31df1e4d0c293ae08af18186
uli <uli@378c4bed-2673-4746-83ae-d22ddc8c5b7c>
2009-08-02 07:58:51 +0000

    Removing the old TADS compiler and an intermediate version of frobtads.
    git-svn-id: svn://witness.is-a-geek.org/svn@25 378c4bed-2673-4746-83ae-d22ddc8c5b7c

--- frobtads/src/osbeos.h
+++
@@ -1,5 +0,0 @@
-/* This gets included if we build FrobTADS in BeOS.
- *
- * It seems nothing special is required here.
- */
-#include "osfrobtads.h"
--- frobtads/src/oswin.h
+++
@@ -1,4 +0,0 @@
-/* This gets included if we build FrobTADS in MS-Windows.
- */
-/* DOS definitions are valid for Windows too, so just include that. */
-#include "osdos.h"
